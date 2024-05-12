f1082e9f175f8da40d1055bbf0351ea97f8cbb99
aryanchy449 <108731844+aryanchy449@users.noreply.github.com>
2024-05-12 15:38:02 +0530

    Update bot.js

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -3,6 +3,11 @@ const startAction = require('./actions/start')
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
+const message= require('telegraf/filters')
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.start(ctx => {
   return startAction(ctx)
 })
@@ -15,5 +20,4 @@ exports.handler = async event => {
     console.log(e)
     return { statusCode: 400, body: 'This endpoint is meant for bot and telegram communication' };
   }
-
 }
